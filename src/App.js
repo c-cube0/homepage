1b13e126d9935cc8d537cf09164074a2cca7caa6
nariya <nariya@shota-2.local>
2020-10-20 14:19:51 +0900

    プロフィール完成

--- src/App.js
+++ src/App.js
@@ -8,9 +8,20 @@ function App() {
     <div className="App">
 
       <header className="App-header">
-
         <h1 className="titllename">Nariya Kinjyou</h1>
       </header>
+      <div className="mainvisual">
+        <h2 className="profile">プロフィール</h2>
+        <div className="profileitems">
+          <h3>氏名</h3><h2>金城　功弥</h2>
+          <h3>年齢</h3><h3>36才</h3>
+          <h3>住所</h3><h3>埼玉県</h3>
+          <h3>趣味</h3><h3>海外ドラマ、ゲーム</h3>
+        </div>
+        <div className="workhistory">
+
+        </div>
+      </div>
     </div>
   );
 }
